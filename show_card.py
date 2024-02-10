d9d1bc39c97dad83ca29ee4e507c7673c1de47a6
aman0103 <aman0103@student.monash.edu>
2024-02-10 23:52:47 +1100

    Implemented show_card function

---
+++ show_card.py
@@ -0,0 +1,12 @@
+def show_card(player_cards):
+    """
+    This function displays all the cards with the player
+
+    Parameters:
+    :param player_cards: List of cards allocated to the player
+
+    Returns:
+    Prints all the cards that belongs to the player
+    """
+    print("Here is your set of cards mate!!!")
+    print(",".join(player_cards))
